e360e83d86d196d21eed3b6f2186db0dd85f7449
8pxl <keijayhuang@gmail.com>
2022-06-28 00:48:18 -0700

    updated controls

--- 2496r/src/main.cpp
+++ 2496r/src/main.cpp
@@ -12,6 +12,24 @@
 // horizEncoder         encoder       C, D            
 
 #include "vex.h"
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// leftFly              motor         16              
+// rightFly             motor         15              
+// Controller1          controller                    
+// frontLeft            motor         3               
+// frontRight           motor         9               
+// backLeft             motor         5               
+// backRight            motor         10              
+// vertEncoder          encoder       A, B            
+// imu                  inertial      20              
+// horizEncoder         encoder       C, D            
+// rightIntake          motor         4               
+// leftIntake           motor         11              
+// indexer              digital_out   E               
+// ---- END VEXCODE CONFIGURED DEVICES ----
 #include <cmath>
 #include "includes.cpp"
 #include "odom.cpp"
@@ -31,9 +49,9 @@ void autonomous(void) {
 void usercontrol(void) {
   bool pressed = true;
   imu.calibrate();
-  wait(3,sec);
+  // wait(3,sec);
   vex::thread t(odom);
-  // spinTo(90,7000,1);
+  // spinTo(90,70001);
   printf("%f", absoluteAngleToPoint(0,0));
   // spinTo(180,5000,1);
   // spinTo(270,5000,1);
@@ -57,8 +75,23 @@ void usercontrol(void) {
 
       if (Controller1.ButtonR2.pressing()){
 
-        spinTo(absoluteAngleToPoint(0,0), 50000, 0);
-        // spinTo(190,5000,5);
+        // spinTo(absoluteAngleToPoint(0,0), 50000, 0);
+        spinTo(90,5000,5);
+        pressed = true;
+        
+      }
+      if(Controller1.ButtonL2.pressing()){
+        indexer.set(true);
+        wait(40,msec);
+        indexer.set(false);
+        wait(40,msec);
+        indexer.set(true);
+        wait(40,msec);
+        indexer.set(false);
+        wait(40,msec);
+        indexer.set(true);
+        wait(40,msec);
+        indexer.set(false);
         pressed = true;
         
       }
@@ -75,12 +108,22 @@ void usercontrol(void) {
       backRight.spin(directionType::fwd,(Controller1.Axis3.value() - Controller1.Axis1.value()) *(0.094),voltageUnits::volt);
       pressed = false;
     }
+
+    if(Controller1.ButtonR1.pressing()){
+      rightIntake.spin(reverse,100,pct);
+      leftIntake.spin(fwd,100,pct);
+    }
+    else{
+      rightIntake.stop(coast);
+      leftIntake.stop(coast);
+    }
   
 
     
-    leftFly.spin(forward,averageRpm()+ fPid(550), velocityUnits::rpm);
-    rightFly.spin(reverse, averageRpm() + fPid(550), velocityUnits::rpm);
-    // printf("%i\n", averageRpm());
+    leftFly.spin(reverse,600 , velocityUnits::rpm);
+    rightFly.spin(reverse,600, velocityUnits::rpm);
+
+    printf("%f\n", leftFly.velocity(rpm));
     // printf("%i\n", rpmTarget);
     // printf("%s\n",' ');
     wait(20, msec);
