8d33c6a363b7cf2c09bd1c7ba4b37e1a0baf39e5
8pxl <keijayhuang@gmail.com>
2022-07-01 00:37:07 -0700

    preparing to delete file

--- 2496r/include/includes.cpp
+++ 2496r/include/includes.cpp
@@ -1,22 +1,35 @@
 #include "vex.h"
 #include <cmath>
+#include "common.h"
 
 float averageRpm(vex::motor m1, vex::motor m2, vex::motor m3, vex::motor m4){
   return ( std::abs(m1.velocity(rpm)) + std::abs(m2.velocity(rpm)) + std::abs(m3.velocity(rpm)) + std::abs(m4.velocity(rpm)) / 4 );
 }
 
-float fPid(float target){
-  float kp = 1.3;
-  int error = target - averageRpm();
-  return error*kp;
+double dtr(double input){
+  return(PI*input/180);
 }
 
-void targetUp(){
-  int offset = 50;
-  rpmTarget = rpmTarget+offset;
+double rtd(double input){
+  return(input*180/PI);
 }
 
-void targetDown(){
-  int offset = 50;
-  rpmTarget = rpmTarget- offset;
+double mod(double a, double b){
+  return fmod(360-std::abs(a), b);
 }
+
+// float fPid(float target){
+//   float kp = 1.3;
+//   int error = target - averageRpm();
+//   return error*kp;
+// }
+
+// void targetUp(){
+//   int offset = 50;
+//   rpmTarget = rpmTarget+offset;
+// }
+
+// void targetDown(){
+//   int offset = 50;
+//   rpmTarget = rpmTarget- offset;
+// }
