89d1f96a1ad172c2b5a0fc190195bfcbf679c900
8pxl <keijayhuang@gmail.com>
2022-07-10 17:04:12 -0700

    added a bunch of utility functions

--- 2496r/include/chassis.cpp
+++ 2496r/include/chassis.cpp
@@ -1,11 +1,88 @@
 #include "vex.h"
 #include "common.h"
 #include <cmath>
-
+#include <tuple>
 
 timer integralTimer;
 timer endTimer;
 timer timeoutTimer;
+
+class coordinate{
+    friend std::ostream& operator<<(std::ostream&, coordinate const&);
+    private:
+    
+    int x;
+    int y;
+    
+    public:
+    
+    coordinate(int a, int b){
+        x = a;
+        y = b;
+    }
+    
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
+    
+};
+
+
+std::tuple<int, int> moveToVel(coordinate target){
+
+  // constants;
+  double lkp = 0.1;
+  double rkp = 0.1;
+
+  double tx = target.getX();
+  double ty = target.getY();
+
+  // linear stuffs.
+  double linearError = distToPoint(x,y,tx, ty);
+  double linearVel = linearError*lkp;
+
+  // rotation stuffs.
+  double currHeading = imu.heading();
+  double targetHeading = rtd(absoluteAngleToPoint(tx, ty));
+  int dir = dirToSpin(targetHeading,currHeading);
+  double rotationError = std::abs(targetHeading - currHeading);
+  double rotationVel = rotationError * rkp * dir;
+
+  //velocities
+  double lVel = linearVel - rotationVel;
+  double rVel = linearVel + rotationVel;
+
+  return std::make_tuple(lVel,rVel);
+}
+
+void spinChassis(double lVel,double rVel){
+    frontLeft.spin(fwd,lVel,rpm);
+    backLeft.spin(fwd,lVel,rpm);
+    frontRight.spin(fwd,rVel,rpm);
+    backRight.spin(fwd,rVel,rpm);
+}
+
+void stopChassis(){
+  frontLeft.stop(brakeType::hold);
+  backLeft.stop(brakeType::hold);
+  frontRight.stop(brakeType::hold);
+  backRight.stop(brakeType::hold);
+}
+
+void moveTo(coordinate target, double timeout){
+    double lVel;
+    double rVel;
+    timeoutTimer.reset();
+
+    while (timeoutTimer.value() < timeout){
+        std::tie(lVel,rVel) = moveToVel(target);
+        spinChassis(lVel, rVel);
+    }
+    stopChassis();
+}
 
 int dirToSpin(double target, double unscaled){
   // -1 for clockwise
@@ -138,92 +215,92 @@ void spinTo(double target, double timeout, double tolerance){
 
 } 
 
-void moveDist(double target, double timeout, double tolerance){
-  //resetting timers
-  integralTimer.reset();
-  endTimer.reset();
-  timeoutTimer.reset();
+// void moveDist(double target, double timeout, double tolerance){
+//   //resetting timers
+//   integralTimer.reset();
+//   endTimer.reset();
+//   timeoutTimer.reset();
 
-  //resetting sensors
-  frontLeft.resetRotation();
-  backLeft.resetRotation();
-  frontRight.resetRotation();
-  backRight.resetRotation();
+//   //resetting sensors
+//   frontLeft.resetRotation();
+//   backLeft.resetRotation();
+//   frontRight.resetRotation();
+//   backRight.resetRotation();
 
-  //basic constants
-  double kP = 2.1;
-  double kI = 0;
-  double kD = 0.1;
-  double endTime = 1;
+//   //basic constants
+//   double kP = 2.1;
+//   double kI = 0;
+//   double kD = 0.1;
+//   double endTime = 1;
 
-  //general vars
-  double currRotation = 0;
-  double error;
-  double prevError;
-  bool end = false;
+//   //general vars
+//   double currRotation = 0;
+//   double error;
+//   double prevError;
+//   bool end = false;
 
-  //eye vars
-  double integral = 0;
-  double integralTimeout = 0;
-  double errorThreshold = 30;
+//   //eye vars
+//   double integral = 0;
+//   double integralTimeout = 0;
+//   double errorThreshold = 30;
 
-  //dee vars
-  double derivative;
-  //pid loop 
-  while (!end){
-    currRotation = averageRpm(frontLeft, backLeft, frontRight, backRight);
+//   //dee vars
+//   double derivative;
+//   //pid loop 
+//   while (!end){
+//     currRotation = averageRpm(frontLeft, backLeft, frontRight, backRight);
 
-    //pee
-    error = target - currRotation;
+//     //pee
+//     error = target - currRotation;
 
-    //eye
-    if (error > errorThreshold){
-      integral += error;
-    } 
-    if (error <= tolerance && integralTimer.value() > integralTimeout){
-      integral = 0;
-    }
-    else if (error >= tolerance ){
-      integralTimer.reset();
-    }
+//     //eye
+//     if (error > errorThreshold){
+//       integral += error;
+//     } 
+//     if (error <= tolerance && integralTimer.value() > integralTimeout){
+//       integral = 0;
+//     }
+//     else if (error >= tolerance ){
+//       integralTimer.reset();
+//     }
 
-    //dee
-    derivative = error - prevError;
+//     //dee
+//     derivative = error - prevError;
     
-    //spin motors
+//     //spin motors
 
-    //double fwd is spin cw
+//     //double fwd is spin cw
 
-    double rVel =  (error*kP + integral*kI + derivative*kD);
-    double lVel =  -(error*kP + integral*kI + derivative*kD);
+//     double rVel =  (error*kP + integral*kI + derivative*kD);
+//     double lVel =  -(error*kP + integral*kI + derivative*kD);
 
-    frontLeft.spin(fwd,lVel,rpm);
-    backLeft.spin(fwd,lVel,rpm);
-    frontRight.spin(fwd,rVel,rpm);
-    backRight.spin(fwd,rVel,rpm);
+//     frontLeft.spin(fwd,lVel,rpm);
+//     backLeft.spin(fwd,lVel,rpm);
+//     frontRight.spin(fwd,rVel,rpm);
+//     backRight.spin(fwd,rVel,rpm);
     
-    //end condition
-    // printf("%lu\n", endTimer.time()/1000);
-    if (error >= tolerance){
-      endTimer.reset();
-    }
+//     //end condition
+//     // printf("%lu\n", endTimer.time()/1000);
+//     if (error >= tolerance){
+//       endTimer.reset();
+//     }
 
-    if (endTimer.time() >= endTime){
-      end = true;
+//     if (endTimer.time() >= endTime){
+//       end = true;
 
-    } 
+//     } 
 
-      if (timeoutTimer.time() >= timeout){
-        end = true;
-      }
+//       if (timeoutTimer.time() >= timeout){
+//         end = true;
+//       }
 
-    prevError = error;
-    wait(15,msec);
-  }
+//     prevError = error;
+//     wait(15,msec);
+//   }
 
-  frontLeft.stop(brakeType::hold);
-  backLeft.stop(brakeType::hold);
-  frontRight.stop(brakeType::hold);
-  backRight.stop(brakeType::hold);
+//   frontLeft.stop(brakeType::hold);
+//   backLeft.stop(brakeType::hold);
+//   frontRight.stop(brakeType::hold);
+//   backRight.stop(brakeType::hold);
 
-} 
+// } 
