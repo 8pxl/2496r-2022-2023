97d921b3459c626c1319a53777c2ca16f018ff1e
8pxl <keijayhuang@gmail.com>
2022-08-20 20:14:40 -0700

    moveToPose utilizing bezier curves
    needs some work still.

--- v2/src/chassis.hpp
+++ v2/src/chassis.hpp
@@ -1,5 +1,6 @@
 #include "global.hpp"
 #include "util.hpp"
+#include <vector>
 
 namespace chassis
 {
@@ -82,8 +83,8 @@ namespace chassis
 
     //general vars
     double dist = -distance;
-    double tx = sin(2*PI-glb::imu.get_heading()) * dist + glb::x;
-    double ty = cos(2*PI-glb::imu.get_heading()) * dist + glb::y;
+    double tx = sin(2*PI-glb::imu.get_heading()) * dist + glb::pos.x;
+    double ty = cos(2*PI-glb::imu.get_heading()) * dist + glb::pos.y;
     double prevRotation;
     double error;
     double prevError;
@@ -101,7 +102,7 @@ namespace chassis
     {
       
       //pee
-      error = dist - (dist-util::distToPoint(glb::x,glb::y,tx,ty));
+      error = dist - (dist-util::distToPoint(util::coordinate (glb::x, glb::y),util::coordinate(tx,ty)));
 
       //eye
       integral = error <= tolerance ? 0 : fabs(error) < integralThreshold ? integral += error : integral;
@@ -127,5 +128,55 @@ namespace chassis
     robot::chass.stop("b");
   }  
 
-  
+  std::vector<double> moveToVel(util::coordinate target)
+  {
+    double lkp = 0.1;
+    double rkp = 0.1;
+    double krp = 1.2;
+
+    double linearError = distToPoint(glb::pos,target);
+    double linearVel = linearError*lkp;
+
+    double currHeading =  glb::imu.get_heading() > 0 ? util::rtd(glb::imu.get_heading()) : util::rtd(glb::imu.get_heading()) + 360; //0-360
+    double targetHeading = 180-absoluteAngleToPoint(target); // -180-180
+
+    targetHeading = targetHeading >= 0 ? targetHeading : fabs(targetHeading) + 180;
+
+    int dir = util::dirToSpin(targetHeading,currHeading);
+
+    double rotationError = util::minError(targetHeading,currHeading);
+    double rotationVel = rotationError * rkp * dir; 
+
+    double lVel = (linearVel - fabs(rotationVel) * krp) - rotationVel;
+    double rVel = (linearVel - fabs(rotationVel) * krp) + rotationVel;
+
+    return std::vector<double> {lVel, rVel};
+  }
+
+  void moveToPosePID(util::coordinate target, double finalHeading, double initialBias, double finalBias, double timeout, double initialHeading = glb::imu.get_heading())
+  {
+
+    util::bezier curve = util::bezier(glb::pos,target,initialBias,finalBias, util::dtr(initialHeading),util::dtr(finalHeading));
+
+    std::vector<util::coordinate> lut = curve.createLUT(100);
+
+    bool run = true;
+    int i = 0;
+    double incrementDist = 0.01;
+    util::coordinate targetPos = lut[i];
+
+    while(run)
+    {
+      if(util::distToPoint(glb::pos, targetPos) <= incrementDist)
+      {
+        i++;
+      }
+
+      targetPos = lut[i];
+
+      std::vector<double> velocities = moveToVel(targetPos);
+      robot::chass.spinDiffy(velocities[1], velocities[0]);
+    }
+  }
+
 }
