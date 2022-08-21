993baa6121811feec07312083b7b341dbce6c654
8pxl <keijayhuang@gmail.com>
2022-08-20 17:05:44 -0700

    bezier class

--- v2/src/util.hpp
+++ v2/src/util.hpp
@@ -9,6 +9,7 @@ namespace util
 {
     class timer;
     class coordinate;
+    class pose;
     class bezier;
     double dtr(double input);
     double rtd(double input);
@@ -51,89 +52,106 @@ class util::coordinate
         coordinate();
 };
 
-// class util::bezier
-// {
-//     private:
-//         coordinate p0;
-//         coordinate p1;
-//         coordinate p2;
-//         coordinate p3;
-//         double initialWeight;
-//         double finalWeight;
-//         double initialHeading;
-//         double finalHeading;
+class util::pose
+{
+    public:
 
-//     public:
-//         bezier(coordinate first, coordinate last, double initialWeight, double finalWeight, double initialHeading, double finalHeading)
-//         {
-//             p0 = first;
-//             p1 = coordinate(first.x + sin(initialHeading) * initialWeight, first.y + cos(initialHeading) * initialWeight);
-//             p2 = coordinate(last.x + sin(PI/2 + (PI/2-finalHeading)) * -1 * finalWeight, last.y + cos(PI/2 + (PI/2-finalHeading)) * -1*finalWeight);
-//             p3 = last;
-//         }
+        util::coordinate pos;
+        double heading;
+
+        pose(util::coordinate p, double h) : pos(p), heading(h){}
+};
+
+class util::bezier
+{
+    private:
+        coordinate p0;
+        coordinate p1;
+        coordinate p2;
+        coordinate p3;
+        double initialWeight;
+        double finalWeight;
+        double initialHeading;
+        double finalHeading;
+
+    public:
+        bezier(coordinate first, coordinate last, double initialWeight, double finalWeight, double initialHeading, double finalHeading)
+        {
+            p0 = first;
+            p1 = coordinate(first.x + sin(initialHeading) * initialWeight, first.y + cos(initialHeading) * initialWeight);
+            p2 = coordinate(last.x + sin(PI/2 + (PI/2-finalHeading)) * -1 * finalWeight, last.y + cos(PI/2 + (PI/2-finalHeading)) * -1*finalWeight);
+            p3 = last;
+        }
         
-//         coordinate solve(double t)
-//         {
-//             double omt = 1-t;
-//             double x0 = p0.x;
-//             double x1 = p1.x;
-//             double x2 = p2.x;
-//             double x3 = p3.x;
-//             double y0 = p0.y;
-//             double y1 = p1.y;
-//             double y2 = p2.y;
-//             double y3 = p3.y;
-//             return coordinate( pow(omt,3) * x0 + 3 * pow(omt,2) * t * x1 + 3*omt * pow(t,2) * x2 + pow(t,3) * x3, pow(omt,3) * y0 + 3 * pow(omt,2) * t * y1 + 3*omt * pow(t,2) * y2 + pow(t,3) * y3);
-//         }
+        coordinate solve(double t)
+        {
+            double omt = 1-t;
+            double x0 = p0.x;
+            double x1 = p1.x;
+            double x2 = p2.x;
+            double x3 = p3.x;
+            double y0 = p0.y;
+            double y1 = p1.y;
+            double y2 = p2.y;
+            double y3 = p3.y;
+            return coordinate( pow(omt,3) * x0 + 3 * pow(omt,2) * t * x1 + 3*omt * pow(t,2) * x2 + pow(t,3) * x3, pow(omt,3) * y0 + 3 * pow(omt,2) * t * y1 + 3*omt * pow(t,2) * y2 + pow(t,3) * y3);
+        }
         
-//         double tangentLineAngle(double t)
-//         {
-//             double x0 = p0.x;
-//             double x1 = p1.x;
-//             double x2 = p2.x;
-//             double x3 = p3.x;
-//             double y0 = p0.y;
-//             double y1 = p1.y;
-//             double y2 = p2.y;
-//             double y3 = p3.y;
+        // double tangentLineAngle(double t)
+        // {
+        //     double x0 = p0.x;
+        //     double x1 = p1.x;
+        //     double x2 = p2.x;
+        //     double x3 = p3.x;
+        //     double y0 = p0.y;
+        //     double y1 = p1.y;
+        //     double y2 = p2.y;
+        //     double y3 = p3.y;
 
-//             ex = symbols('x')
-//             omt = 1-ex
-//             # bx = pow(omt,3) * x0 + 3 * pow(omt,2) * t * x1 + 3*omt * pow(t,2) * x2 + pow(t,3) * x3
-//             # by = pow(omt,3) * y0 + 3 * pow(omt,2) * t * y1 + 3*omt * pow(t,2) * y2 + pow(t,3) * y3
+        //     ex = symbols('x')
+        //     omt = 1-ex
+        //     # bx = pow(omt,3) * x0 + 3 * pow(omt,2) * t * x1 + 3*omt * pow(t,2) * x2 + pow(t,3) * x3
+        //     # by = pow(omt,3) * y0 + 3 * pow(omt,2) * t * y1 + 3*omt * pow(t,2) * y2 + pow(t,3) * y3
 
-//             bx = pow(omt,3) * x0 + 3 * pow(omt,2) * ex * x1 + 3*omt * pow(ex,2) * x2 + pow(ex,3) * x3
-//             by = pow(omt,3) * y0 + 3 * pow(omt,2) * ex * y1 + 3*omt * pow(ex,2) * y2 + pow(ex,3) * y3
+        //     bx = pow(omt,3) * x0 + 3 * pow(omt,2) * ex * x1 + 3*omt * pow(ex,2) * x2 + pow(ex,3) * x3
+        //     by = pow(omt,3) * y0 + 3 * pow(omt,2) * ex * y1 + 3*omt * pow(ex,2) * y2 + pow(ex,3) * y3
             
 
-//             bpx = diff(bx,ex).evalf(subs={ex: t})
-//             bpy = diff(by,ex).evalf(subs={ex: t})
+        //     bpx = diff(bx,ex).evalf(subs={ex: t})
+        //     bpy = diff(by,ex).evalf(subs={ex: t})
 
-//             m = bpy/bpx
+        //     m = bpy/bpx
 
-//             return(atan2(1,m))
-//         }
+        //     return(atan2(1,m))
+        // }
 
-//         std::vector<double> createLUT(double resolution)
-//         {
-//             std::vector<coordinate> points;
-//             std::vector<double> angles;
+        std::vector<coordinate> createLUT(double resolution)
+        {
+            std::vector<coordinate> points;
+            // std::vector<double> angles;
 
-//             for (int i=1; i<= resolution + 1; i++)
-//             {
-//                 points.push_back(solve(i/resolution));
-//                 angles.push_back(tangentLineAngle(i/resolution));
-//             }
-//             return (points, angles);
-//         }
-//         double approximateLength(lut, resolution):
-//             length = 0
-//             for i in range(resolution - 1):
-//                 p0 = lut[0][i]
-//                 p1 = lut[0][i+1]
-//                 length += distToPoint(p0[0],p0[1],p1[0],p1[1])
-//             return length
-// };
+            for (int i=0; i < resolution; i++)
+            {
+                points.push_back(solve(i/resolution));
+                // angles.push_back(tangentLineAngle(i/resolution));
+            }
+
+            return (points);
+        }
+
+        double approximateLength(std::vector<coordinate> lut, double resolution)
+        {
+            double length = 0;
+
+            for (int i=0; i < resolution; i++)
+            {
+                p0 = lut[i];
+                p1 = lut[i+1];
+                length += distToPoint(p0.x,p0.y,p1.x,p1.y);
+            }
+            return length;
+        }
+};
 
 double util::dtr(double input)
 {
