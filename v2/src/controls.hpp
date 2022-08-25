398718afe9dbf20968b2c3dfcd992e75f389e846
8pxl <keijayhuang@gmail.com>
2022-08-24 21:04:09 -0700

    fw and intake control

--- v2/src/controls.hpp
+++ v2/src/controls.hpp
@@ -1,13 +1,41 @@
 #include "main.h"
 #include "global.hpp"
+#include "pros/misc.h"
 
 void driveContol()
 {
-    //chassis
+    // chassis
     double lStick = glb::controller.get_analog(ANALOG_LEFT_Y);
     double rStick = glb::controller.get_analog(ANALOG_RIGHT_X);
     robot::chass.spinDiffy(lStick+rStick,lStick-rStick);
 
-    //intake
+    // intake
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+    {
+        robot::intake.spin(127);
+    }
 
+    else if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+    {
+        robot::intake.spin(-127);
+    }
+
+    else
+    {
+        robot::intake.stop("c");
+    }
+
+    // fw
+
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
+    {
+        flywheel::target = 600;
+    }
+
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
+    {
+        flywheel::target = 400;
+    }
+
+    flywheel::target -= 1;
 }
