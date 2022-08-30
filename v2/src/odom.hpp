e46af7e171d5da0fad6437746effcd3fc6aff4c6
8pxl <keijayhuang@gmail.com>
2022-08-29 20:11:24 -0700

    Update odom.hpp

--- v2/src/odom.hpp
+++ v2/src/odom.hpp
@@ -15,14 +15,15 @@ void odom()
     double trackingDiameter = 2.75;
     double trackingCirumfrence = 2.75 * PI;
     double scaleFactor = 5.3625;
+    double offset = 0 * scaleFactor;
 
     while(1)
     {
-        // printf("(%f,%f)\n",glb::pos.x, glb::pos.y);
+        // glb::controller.print(0,0,"(%f, %f)\n", glb::pos.x,glb::pos.y);
 
         // calcualting change in rotation
         double currRotation = glb::imu.get_heading();
-        double deltaRotation = util::dtr(currRotation - prevRotation);
+        double deltaRotation = currRotation - prevRotation;
 
         /* when angle difference jumps by more than 300, it can be assumed that it is caused by the imu rotating past
         0 to 360 or from 360 to 0. in order toget the absolute difference in rotation, the mod of bothvalues is taken.
@@ -32,46 +33,47 @@ void odom()
 
         if (std::abs(deltaRotation) > 300)
         {
-            deltaRotation = util::dtr(util::mod(currRotation,360) - util::mod(prevRotation,360));
+            deltaRotation = (util::mod(currRotation,360) - util::mod(prevRotation,360));
         }
+
         prevRotation = currRotation;
 
-        // change in encoder value
-        double le = glb::leftEncoder.get_value();
-        double he = glb::leftEncoder.get_value();
-        double deltaVert = le != 0 ? (trackingCirumfrence/le) : 0  * scaleFactor;
-        double deltaHoriz = he != 0 ? (trackingCirumfrence/he) : 0 * scaleFactor;
+        deltaRotation = util::dtr(deltaRotation);
+        currRotation = util::dtr(currRotation);
 
+        // change in encoder value
+        double deltaVert = (trackingCirumfrence / 360) * glb::leftEncoder.get_value() * scaleFactor;
+        double deltaHoriz = (trackingCirumfrence / 360) * glb::horizEncoder.get_value() * scaleFactor;
+
+        
         if (deltaRotation == 0)
         {
-            deltaY = cos(2*PI-currRotation) * le;
-            deltaX = sin(2*PI-currRotation) * le;
+            deltaY = cos(2*PI-currRotation) * deltaVert;
+            deltaX = sin(2*PI-currRotation) * deltaVert;
         }
+
         else
         {
             // calculating change in relative y
-            double sOverTheta = deltaVert / deltaRotation;
+            double sOverTheta = deltaVert / deltaRotation + 0;
             double relativeY = 2*sin(deltaRotation/2) * sOverTheta;
 
             // calculating change in relative x
-            sOverTheta = deltaHoriz/ deltaRotation;
+            sOverTheta = deltaHoriz / deltaRotation + 0;
             double relativeX = 2*sin(deltaRotation/2) * sOverTheta;
 
             // calculing absolute x and y 
-            // rotates the coordinate [relativeX, relativeY] to get an absolute position vector
-            double rotationOffset = util::dtr(currRotation)+deltaRotation/2;
+            double rotationOffset = currRotation + (deltaRotation/2);
 
             double theta = atan2(relativeY, relativeX);
             double radius = sqrt(relativeX*relativeX + relativeY*relativeY);
-            theta += rotationOffset;
+            theta -= rotationOffset;
             deltaX = radius*cos(theta);
             deltaY = radius*sin(theta);
-
-            // double deltaX = relativeX * cos(rotationOffset) - relativeY* sin(rotationOffset);
-            // double deltaY = relativeX * sin(rotationOffset) + relativeY* cos(rotationOffset);
         }
 
         // updating global x and global y
+        // glb::controller.print(0,0,"(%f, %f)\n", deltaX,deltaY);
         glb::pos.x -= deltaX;
         glb::pos.y -= deltaY;
 
