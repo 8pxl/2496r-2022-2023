8633706fc3aae1f120cd288946d62e980da5c445
8pxl <keijayhuang@gmail.com>
2022-08-29 20:10:42 -0700

    revamped fw control

--- v2/src/controls.hpp
+++ v2/src/controls.hpp
@@ -1,7 +1,10 @@
 #include "main.h"
 #include "global.hpp"
+#include "chassis.hpp"
 #include "pros/misc.h"
+#include "util.hpp"
 
+util::timer decelTimer; 
 void driveContol()
 {
     // chassis
@@ -22,20 +25,67 @@ void driveContol()
 
     else
     {
+        // rollers::spin();
         robot::intake.stop("c");
     }
 
     // fw
 
-    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
-    {
-        flywheel::target = 600;
-    }
-
     if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
     {
-        flywheel::target = 400;
+        if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+        {
+            flywheel::target = 500;
+        }
+
+        else
+        {
+            flywheel::target = 600;
+        }
+
+        decelTimer.start();
     }
 
-    flywheel::target -= 1;
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
+    {
+        if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+        {
+            flywheel::target = 300;
+        }
+
+        else
+        {
+            flywheel::target = 400;
+        }
+        decelTimer.start();
+    }
+
+    //aut
+
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_A))
+    {
+        chas::spinTo(90, 2000, 1);
+        chas::drive(1200, 3000, 20);
+        // chas::moveTo(util::coordinate(0,0), 100000, 0.6, 0.4, 1);
+    }
+
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_B))
+    {
+        double target = absoluteAngleToPoint(glb::pos, util::coordinate(0,0));
+        target = target >= 0 ? target : 180 + (180 - fabs(target)); //conver to 0-360
+        glb::controller.print(0, 0, "%f", target);
+
+        // chas::spinTo(135, 1000, 1);
+        chas::spinTo(target, 5000, 1);
+    }
+    
+
+    // glb::controller.print(0,0, "%f", glb::imu.get_heading());
+    util::coordinate a = util::coordinate(0,0);
+    // chas::moveToVel(a, 0, 0,  0);
+
+    if (decelTimer.time() > 3000)
+    {
+        flywheel::target > 300 ? flywheel::target -= 0.5 : flywheel::target = 300;
+    }
 }
