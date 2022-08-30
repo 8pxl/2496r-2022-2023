4fe5cefda7563c6e93373661417c7d2134744637
8pxl <keijayhuang@gmail.com>
2022-08-29 21:43:58 -0700

    added auton selector

---
+++ v2/src/master.hpp
@@ -0,0 +1,131 @@
+#include "main.h"
+#include "global.hpp"
+#include "chassis.hpp"
+#include "util.hpp"
+#include "autons.hpp"
+
+util::timer decelTimer; 
+
+void driveContol()
+{
+    // chassis
+    double lStick = glb::controller.get_analog(ANALOG_LEFT_Y);
+    double rStick = glb::controller.get_analog(ANALOG_RIGHT_X);
+    robot::chass.spinDiffy(lStick+rStick,lStick-rStick);
+
+    // intake
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+    {
+        robot::intake.spin(127);
+        rollers::noInput = false;
+    }
+
+    else if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+    {
+        robot::intake.spin(-127);
+        rollers::noInput = false;
+    }
+
+    else
+    {
+        rollers::noInput = true;
+        // rollers::spin();
+        // robot::intake.stop("c");
+    }
+
+    // fw
+
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
+    {
+        if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+        {
+            flywheel::target = 500;
+        }
+
+        else
+        {
+            flywheel::target = 600;
+        }
+
+        decelTimer.start();
+    }
+
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
+    {
+        if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+        {
+            flywheel::target = 300;
+        }
+
+        else
+        {
+            flywheel::target = 400;
+        }
+        decelTimer.start();
+    }
+
+    //aut
+
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_A))
+    {
+        chas::spinTo(90, 2000, 1);
+        chas::drive(1200, 3000, 20);
+        // chas::moveTo(util::coordinate(0,0), 100000, 0.6, 0.4, 1);
+    }
+
+    if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_B))
+    {
+        double target = absoluteAngleToPoint(glb::pos, util::coordinate(0,0));
+        target = target >= 0 ? target : 180 + (180 - fabs(target)); //conver to 0-360
+        glb::controller.print(0, 0, "%f", target);
+
+        // chas::spinTo(135, 1000, 1);
+        chas::spinTo(target, 5000, 1);
+    }
+    
+
+    // glb::controller.print(0,0, "%f", glb::imu.get_heading());
+    util::coordinate a = util::coordinate(0,0);
+    // chas::moveToVel(a, 0, 0,  0);
+
+    if (decelTimer.time() > 3000)
+    {
+        flywheel::target > 300 ? flywheel::target -= 0.5 : flywheel::target != 0 ? flywheel::target = 300 : flywheel::target = 0;
+    }
+}
+
+int autonSelector()
+{
+    int numAutons = autons.size();
+    int selectedAut = 0;
+    bool autSelected = false;
+
+    robot::flywheel.reset();
+
+    while(!autSelected)
+    {   
+
+        if(robot::flywheel.getRotation() >= 200)
+        {
+            selectedAut = selectedAut == numAutons ? 0 : selectedAut++;
+            robot::flywheel.spin(-2);
+            robot::flywheel.reset();
+        }
+
+        if(robot::flywheel.getRotation() <= -200)
+        {
+            selectedAut = selectedAut == 0 ? numAutons : selectedAut --;
+            robot::flywheel.spin(-2);
+            robot::flywheel.reset();
+        }
+
+        if(glb::controller.get_digital(pros::E_CONTROLLER_DIGITAL_A))
+        {
+            autSelected = true;
+        }
+
+        glb::controller.print(0, 0, "%s", autonNames[numAutons]);
+    }
+
+    return(selectedAut);
+}
