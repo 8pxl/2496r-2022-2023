b4c15819e6bdef9e1dea74104e3104be1645ee56
8pxl <keijayhuang@gmail.com>
2022-09-02 14:30:25 -0700

    improved roller and index functions

--- v2/src/intake.hpp
+++ v2/src/intake.hpp
@@ -1,12 +1,75 @@
 #include "global.hpp"
+#include "flywheel.hpp"
 
-void index(int num)
+namespace intake
 {
-    for (int i = 0; i < num; i++)
+    // void index(int num)
+    // {
+    //     for (int i = 0; i < num; i++)
+    //     {
+    //         robot::intake.spin(-50);
+    //         pros::delay(250);
+    //         robot::intake.stop("b");
+    //         pros::delay(100);
+    //     }
+    // }
+
+    void index(int num)
     {
-        robot::intake.spin(-50);
-        pros::delay(250);
-        robot::intake.stop("b");
-        pros::delay(100);
+        for (int i = 0; i < num; i++)
+        {
+            while (true)
+            {
+                if(flywheel::target - robot::flywheel.getSpeed() > 30)
+                {
+                    robot::intake.stop("b");
+                }
+
+                else
+                {
+                    robot::intake.spin(-50);
+                }
+            }
+
+        }
+    }
+
+    void toggle()
+    {
+        if(glb::red)
+        {   
+            while (true)
+            {
+                if(glb::optical.get_hue() >= 200)
+                {
+                    robot::intake.spin(80);
+                }
+
+                else if(glb::optical.get_hue() <= 10)
+                {
+                    robot::intake.spin(-80);
+                    pros::delay(500);
+                    break;
+                }
+            }
+        }
+
+        else
+        {   
+            while (true)
+            {
+                if(glb::optical.get_hue() >= 200)
+                {
+                    robot::intake.spin(-80);
+                    pros::delay(500);
+                    break;
+                }
+
+                else if(glb::optical.get_hue() <= 10)
+                {
+                    robot::intake.spin(80);
+                }
+            }
+        }
     }
 }
