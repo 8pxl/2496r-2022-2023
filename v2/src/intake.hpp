7a7e7d22aa1f87301a189969c093dcb8641a9a40
8pxl <keijayhuang@gmail.com>
2022-09-07 20:52:25 -0700

    revamped auto toggle
    (still doesnt work)

--- v2/src/intake.hpp
+++ v2/src/intake.hpp
@@ -34,41 +34,79 @@ namespace intake
         }
     }
 
-    void toggle()
+    void spinUntil(double c1, double c2, double speed)
     {
-        if(glb::red)
-        {   
-            while (true)
+        //c1 is curr color
+        while (true)
+        {
+            if (c1 < c2)
             {
-                if(glb::optical.get_hue() >= 200)
+                robot::chass.spin(100);
+                if(glb::optical.get_hue() <= c1)
                 {
-                    robot::intake.spin(80);
+                    robot::intake.spin(speed);
                 }
 
-                else if(glb::optical.get_hue() <= 10)
+                else if(glb::optical.get_hue() >= c2)
                 {
-                    robot::intake.spin(-80);
-                    pros::delay(500);
+                    // robot::intake.spin(-speed);
+                    // pros::delay(150);
                     break;
                 }
+            }
+
+            else
+            {
+                robot::chass.spin(100);
+                if(glb::optical.get_hue() >= c1)
+                {
+                    robot::intake.spin(speed);
+                }
+
+                else if(glb::optical.get_hue() <= c2)
+                {
+                    // robot::intake.spin(-speed);
+                    // pros::delay(150);
+                    break;
+                }
+            }
+        }
+    }
+
+
+    void toggle()
+    {
+
+        double initColor = glb::optical.get_hue();
+        bool initRed = initColor >= 200 ? false : true;
+
+        double red = 10;
+        double blue = 200;
+        double speed = 80;
+
+        if(glb::red)
+        {   
+            if (initRed)
+            {
+                spinUntil(red, blue, -speed);
+            }
+
+            else
+            {
+                spinUntil(blue, red, speed);
             }
         }
 
         else
         {   
-            while (true)
+            if (initRed)
             {
-                if(glb::optical.get_hue() >= 200)
-                {
-                    robot::intake.spin(-80);
-                    pros::delay(500);
-                    break;
-                }
+                spinUntil(red, blue, -speed);
+            }
 
-                else if(glb::optical.get_hue() <= 10)
-                {
-                    robot::intake.spin(80);
-                }
+            else
+            {
+                spinUntil(blue, red, -speed);
             }
         }
     }
