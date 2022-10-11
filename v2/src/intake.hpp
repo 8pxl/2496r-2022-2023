52c25fef2e18bc6662a090f8b7324cf6cb633366
8pxl <keijayhuang@gmail.com>
2022-10-10 20:42:11 -0700

    Update intake.hpp

--- v2/src/intake.hpp
+++ v2/src/intake.hpp
@@ -1,55 +1,63 @@
 #include "global.hpp"
 #include "flywheel.hpp"
 #include "pros/rtos.hpp"
+#include "util.hpp"
 
 namespace intake
 {
-    // void index(int num)
-    // {
-    //     for (int i = 0; i < num; i++)
-    //     {
-    //         robot::intake.spin(-50);
-    //         pros::delay(250);
-    //         robot::intake.stop("b");
-    //         pros::delay(100);
-    //     }
-    // }
-
     void index(int num)
     {
         for (int i = 0; i < num; i++)
         {
-            while (true)
-            {
-                if(flywheel::target - robot::flywheel.getSpeed() > 10)
-                {
-                    robot::intake.stop("b");
-                }
-
-                else
-                {
-                    robot::intake.spinDist(120, 50 , "b");
-                    break;
-                }
-            }
-
+            robot::intake.spin(-50);
+            pros::delay(250);
+            robot::intake.stop("b");
+            pros::delay(100);
         }
     }
+
+    // void index(int num)
+    // {
+    //     for (int i = 0; i < num; i++)
+    //     {
+    //         while (true)
+    //         {
+    //             glb::controller.print(0, 0, "%f", flywheel::target - robot::flywheel.getSpeed());
+    //             if(flywheel::target - robot::flywheel.getSpeed() > 5)
+    //             {
+    //                 robot::intake.stop("b");
+    //             }
+
+    //             else
+    //             {
+    //                 // robot::intake.spinDist(120, 50 , "b");
+    //                 robot::intake.spin(-80);
+    //                 pros::delay(130);
+    //                 robot::intake.stop("b");
+    //                 break;
+    //             }
+    //             pros::delay(10);
+    //         }
+
+    //     }
+    // }
 
     void spinUntil(double color, double speed)
     {
         robot::chass.spin(50);
-        
+
         if(color == 200)
         {
             while(true)
             {
-                robot::intake.spin(127);
+                robot::intake.spin(speed);
 
                 if( glb::optical.get_hue() > color)
                 {
                     break;
                 }
+                
+
             }
         }
 
@@ -57,7 +65,7 @@ namespace intake
         {
             while(true)
             {
-                robot::intake.spin(127);
+                robot::intake.spin(speed);
 
                 if( glb::optical.get_hue() < color)
                 {
@@ -73,13 +81,14 @@ namespace intake
     {
 
         double initColor = glb::optical.get_hue();
+        // glb::controller.print(1, 1, "%f", glb::optical.get_hue());
         bool initRed = initColor >= 200 ? false : true;
 
         double red = 10;
         double blue = 200;
-        double speed = 80;
+        double speed = 90;
 
-        if (glb::red)
+        if (!glb::red)
         {
             if(initRed)
             {
